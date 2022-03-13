953d628e81854bb463d04b429a992d8a5a2c80f7
cjrequena <cjrequena+git@protonmail.com>
2022-03-13 13:12:04 +0100

    --

---
+++ install-apps-mac.sh
@@ -0,0 +1,51 @@
+brew update;
+
+# Browsers
+brew install --cask google-chrome
+brew install --cask tor-browser
+brew install --cask firefox
+
+# IDEs and Editors
+brew install --cask sublime-text
+brew install --cask visual-studio-code
+brew install --cask intellij-idea-ce
+
+# Version Control
+brew install --cask sourcetree
+brew install --cask gitkraken
+
+# VPN
+brew install --cask protonvpn
+brew install --cask openconnect-gui
+
+# Cloud Storage
+brew install --cask box-drive
+
+# 
+brew install --cask docker
+brew install --cask postman
+brew install --cask robo-3t
+brew install --cask offset-explorer
+
+
+#
+brew install --cask alfred
+brew install --cask appcleaner
+brew install --cask gpg-suite
+brew install --cask iterm2
+brew install --cask keka
+brew install --cask calibre
+brew install --cask adobe-acrobat-reader
+brew install --cask microsoft-office
+brew install --cask microsoft-teams
+
+#
+brew install wget
+brew instal  curl
+brew install tree
+brew instal  awscli
+brew install jenv
+brew install nvm
+brew install pyenv
+brew install pyenv-virtualenv
+brew install rbenv
