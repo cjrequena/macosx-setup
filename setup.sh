3823047932de9b84f3440a1a2d2602b8a2ed5896
cjrequena <cjrequena@protonmail.com>
2022-07-15 11:24:08 +0200

    --

--- setup.sh
+++ setup.sh
@@ -10,20 +10,50 @@ WHITE='\033[01;37m'
 BOLD='\033[1m'
 UNDERLINE='\033[4m'
 
+# chmod +x
+echo  "${GREEN}-------------------------- ${NONE}"
+echo  "${GREEN}Giving execution permision to provision shellscripts ${NONE}"
+echo  "${GREEN}-------------------------- ${NONE}"
+chmod +x ./provision/**/*
+echo  "${GREEN}DONE ${NONE}"
+echo  "${GREEN} ${NONE}"
+
+# Set hostname
+echo  "${GREEN}-------------------------- ${NONE}"
+echo  "${GREEN}Setting hostname ${NONE}"
+echo  "${GREEN}-------------------------- ${NONE}"
+sh ./provision/shell/set_hostname.sh
+echo  "${GREEN}DONE ${NONE}"
+echo  "${GREEN} ${NONE}"
+
+
 # Install homebrew
 echo  "${GREEN}-------------------------- ${NONE}"
 echo  "${GREEN}Installing Homebrew ${NONE}"
 echo  "${GREEN}-------------------------- ${NONE}"
 sh ./provision/shell/brew_install.sh
+echo  "${GREEN}DONE ${NONE}"
+echo  "${GREEN} ${NONE}"
 
 # Install ansible
 echo  "${GREEN}-------------------------- ${NONE}"
 echo  "${GREEN}Installing Ansible ${NONE}"
 echo  "${GREEN}-------------------------- ${NONE}"
 sh ./provision/shell/ansible_install.sh
+echo  "${GREEN}DONE ${NONE}"
+echo  "${GREEN} ${NONE}"
 
 # Execute ansible playbook.
 echo  "${GREEN}-------------------------- ${NONE}"
 echo  "${GREEN}Executing ansible playbook ${NONE}"
 echo  "${GREEN}-------------------------- ${NONE}"
 ansible-playbook  provision/ansible/playbook.yml
+echo  "${GREEN} ${NONE}"
+
+# Configuring .zprofile
+echo  "${GREEN}-------------------------- ${NONE}"
+echo  "${GREEN}Configuring .zprofile ${NONE}"
+echo  "${GREEN}-------------------------- ${NONE}"
+cp .zprofile ~/
+echo  "${GREEN}DONE ${NONE}"
+echo  "${GREEN} ${NONE}"
